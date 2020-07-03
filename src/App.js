306bb7d944a7e0eae24ace027fa303fe8e807579
ChinnawatPok <pokas_1998@hotmail.com>
2020-07-03 15:24:40 +0700

    test-one

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to
         </p>
+        <div>pok</div>
         <a
           className="App-link"
           href="https://reactjs.org"
